1996ad41e50b64f76b678ac451d7eee8a4b85749
diglopes <diego-chez@hotmail.com>
2019-10-11 13:02:36 -0300

    feat: adds the loading component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <div id="app">
     <SearchBar />
-    <main class="container">
-      <Breadcrumbs />
+
+    <main class="main">
       <transition name="fade" appear mode="out-in">
-        <router-view />
+        <Loading v-if="loading" />
       </transition>
+
+      <div class="container">
+        <Breadcrumbs />
+        <transition name="fade" appear mode="out-in">
+          <router-view />
+        </transition>
+      </div>
     </main>
   </div>
 </template>
@@ -13,15 +20,25 @@
 <script>
 import SearchBar from "@/components/SearchBar";
 import Breadcrumbs from "@/components/Breadcrumbs";
+import Loading from "@/components/Loading";
 
 export default {
   name: "app",
+  computed: {
+    loading() {
+      return this.$store.state.loading;
+    }
+  },
   components: {
     SearchBar,
-    Breadcrumbs
+    Breadcrumbs,
+    Loading
   }
 };
 </script>
 
 <style lang="scss">
+.main {
+  position: relative;
+}
 </style>
---
+++ frontend/src/components/Loading.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="loading">
+    <div class="loading__wrapper">
+      <span class="loading__ball"></span>
+      <span class="loading__ball"></span>
+      <span class="loading__ball"></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "loading"
+};
+</script>
+
+
+<style lang="scss" scoped>
+.loading {
+  z-index: 100;
+  position: absolute;
+  top: -16px;
+  left: 0;
+  background: rgba($color: #000000, $alpha: 0.5);
+  width: 100%;
+  min-height: calc(100vh - 60px);
+  height: 100%;
+
+  &__wrapper {
+    position: fixed;
+    left: 50%;
+    transform: translate3d(-50%, 0, 0);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 256px;
+  }
+
+  &__ball {
+    display: block;
+    width: 32px;
+    height: 32px;
+    border-radius: 50%;
+    background: var(--clr-primary);
+    margin: 0 8px;
+    animation: bouncing-loader 0.6s infinite alternate;
+
+    &:nth-child(1) {
+      background: darken(#ffe600, 8);
+    }
+
+    &:nth-child(2) {
+      animation-delay: 0.2s;
+      background: darken(#ffe600, 4);
+    }
+
+    &:nth-child(3) {
+      animation-delay: 0.4s;
+    }
+  }
+}
+
+@keyframes bouncing-loader {
+  to {
+    opacity: 0.1;
+    transform: translate3d(0, -1rem, 0);
+  }
+}
+</style>
