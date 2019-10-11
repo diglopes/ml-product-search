edf172da9919a76572b1cfedc399b2929b8982c5
diglopes <diego-chez@hotmail.com>
2019-10-11 05:37:04 -0300

    feat: separates the breadcrumbs into a single component

--- frontend/src/views/SearchResults.vue
+++ frontend/src/views/SearchResults.vue
@@ -1,13 +1,5 @@
 <template>
-  <section class="container">
-    <div class="categories">
-      <span class="categories__item">Eletrônica, Audio e Vídeo</span>
-      <span class="categories__item">IPod</span>
-      <span class="categories__item">Reprodutores</span>
-      <span class="categories__item">IPod Touch</span>
-      <span class="categories__item">32 GB</span>
-    </div>
-
+  <section>
     <ul class="results-container">
       <SearchResultItem />
       <SearchResultItem />
@@ -28,35 +20,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.categories {
-  color: #777;
-  margin: 16px 0;
-  font-size: 14px;
-
-  &__item {
-    margin-right: 8px;
-
-    & + span::before {
-      content: ">";
-      font-weight: normal;
-      font-size: 16px;
-      align-items: center;
-      margin-right: 10px;
-      height: 100%;
-    }
-
-    &:last-of-type {
-      font-weight: bold;
-    }
-  }
-}
-
-@media screen and (max-width: 576px) {
-  .categories {
-    font-size: 12px;
-  }
-}
-
 .results-container {
   background: #fff;
   border-radius: 4px;
