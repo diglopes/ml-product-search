64080446cba0e13f82da2b659057c90c12c18702
diglopes <diego-chez@hotmail.com>
2019-10-11 05:08:21 -0300

    feat: adds the global styles and SearchBar component

---
+++ frontend/src/components/SearchBar.vue
@@ -0,0 +1,64 @@
+<template>
+  <header class="search-bar">
+    <div class="search-bar__wrapper container">
+      <div class="search-bar__logo">
+        <img src="@/assets/images/Logo_ML.png" alt="Mercado Livre" />
+      </div>
+      <form class="search-bar__form">
+        <input type="text" class="search-bar__input" placeholder="Nunca deixe de buscar" />
+        <button class="search-bar__button">
+          <img src="@/assets/images/ic_Search.png" alt="Search" />
+        </button>
+      </form>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "search-bar"
+};
+</script>
+
+<style lang="scss" scoped>
+.search-bar {
+  background: var(--clr-primary);
+  width: 100%;
+  padding: 10px 0;
+
+  &__logo {
+    margin-right: 32px;
+  }
+
+  &__wrapper {
+    display: flex;
+    align-items: center;
+  }
+
+  &__form {
+    display: flex;
+    width: 100%;
+  }
+
+  &__input {
+    flex: 1;
+    border-radius: 4px 0 0 4px;
+  }
+
+  &__button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-size: cover;
+    width: 50px;
+    border: none;
+    background: var(--clr-light);
+    cursor: pointer;
+    border-radius: 0 4px 4px 0;
+
+    &:hover {
+      background: darken(#eee, 3);
+    }
+  }
+}
+</style>
